b87a5f65cdd2ce020ef818e304c342be613f226e
Bryce Billingsley <bryceab@protonmail.com>
2022-11-27 16:34:05 -0600

    still trying

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static("public"));
+app.use(express.static("../frontend/public"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
